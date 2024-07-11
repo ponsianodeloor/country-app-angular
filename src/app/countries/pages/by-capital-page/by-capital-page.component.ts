0f99d82932b036e264eeb45b3d83102a785fc854
Ponsiano De Loor <pdeloor@sudamericano.fin.ec>
2024-07-11 13:22:51 -0500

    exportar valor de un input que se encuentra en un modulo compartido, esto es para que la pagina o componente que lo implemente pueda obtener el valor string de lo emitido

--- src/app/countries/pages/by-capital-page/by-capital-page.component.ts
+++ src/app/countries/pages/by-capital-page/by-capital-page.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
 
 @Component({
   selector: 'app-by-capital-page',
@@ -7,5 +7,13 @@ import { Component } from '@angular/core';
 })
 export class ByCapitalPageComponent {
   public placeholder: string = 'Búsqueda por Capital';
+
+  constructor() {
+  }
+
+  public searchByCapital(query: string): void {
+    console.log(query);
+    console.log('Search by capital page');
+  }
 
 }
--- src/app/shared/components/search-box/search-box.component.ts
+++ src/app/shared/components/search-box/search-box.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input} from '@angular/core';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
 
 @Component({
   selector: 'shared-search-box',
@@ -6,9 +6,18 @@ import {Component, Input} from '@angular/core';
   styleUrl: './search-box.component.css'
 })
 export class SearchBoxComponent {
+
   @Input()
   public placeholder: string = 'Search...';
 
+  @Output()
+  public txtSearch: EventEmitter<string> = new EventEmitter<string>();
+
   constructor() { }
+
+  public onSearch(query: string): void {
+    this.txtSearch.emit(query);
+    console.log('Search box component');
+  }
 
 }
