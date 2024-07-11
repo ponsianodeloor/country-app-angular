859d826ce66208aecab674e618f892bf6a595f13
Ponsiano De Loor <pdeloor@sudamericano.fin.ec>
2024-07-11 10:03:47 -0500

    carga diferida de datos hasta que se requiera su uso combinando spa quienes son componentes de datos que se cargan en primera instancia junto con la aplicacion, con esto evitamos cargar datos al iniciar la aplicacion

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,6 +10,10 @@ const routes: Routes = [
   { path: 'home', component: HomePageComponent },
   { path: 'about', component: AboutPageComponent },
   { path: 'contact', component: ContactPageComponent },
+  {
+    path: 'countries',
+    loadChildren: () => import('./countries/countries.module').then(m => m.CountriesModule)
+  },
   { path: '**', redirectTo: '' }
 
 ];
